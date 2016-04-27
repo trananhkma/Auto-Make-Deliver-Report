1fc55feb42c93db023d3c84bffac82b158a9907c
Trần Tuấn Anh <trananhkma@users.noreply.github.com>
2016-04-27 09:24:18 +0700

    clean up

--- deliver.py
+++ deliver.py
@@ -36,16 +36,15 @@ def get_commit_url(html):
     return html.split('(gitweb)')[0].split('<a href="')[-1].split('"')[0]
 
 
-def get_commit_urls(gerrit_url, html):
+def get_patch_urls(gerrit_url, html):
     patch_count = get_patch_count(html)
-    commit_urls = {}
-    commit_urls[patch_count] = ROOT + get_commit_url(html).replace('commitdiff', 'patch')
+    patch_urls = {}
+    patch_urls[patch_count] = ROOT + get_commit_url(html).replace('commitdiff', 'patch')
     while patch_count > 1:
         patch_count -= 1
-        patch_url = gerrit_url + str(patch_count)
-        html = get_html(patch_url)
-        commit_urls[patch_count] = ROOT + get_commit_url(html).replace('commitdiff', 'patch')
-    return commit_urls
+        _html = get_html(gerrit_url + str(patch_count))
+        patch_urls[patch_count] = ROOT + get_commit_url(_html).replace('commitdiff', 'patch')
+    return patch_urls
 
 
 def get_project_name(html):
@@ -56,7 +55,10 @@ def get_bug_name(html):
     if 'Closes-Bug: #' in html:
         return 'bug_%s' % html.split('Closes-Bug: #')[1].split('</a>')[0]
     elif 'Partial-Bug: #' in html:
-        return 'bug_%s' % html.split('Partial-Bug: #')[1].split('</a>')[0], 'patch_%s' % html.split('Change <a')[1].split('</a>')[0].split('">')[-1]
+        return (
+            'bug_%s' % html.split('Partial-Bug: #')[1].split('</a>')[0],
+            'patch_%s' % html.split('Change <a')[1].split('</a>')[0].split('">')[-1]
+        )
     else:
         return 'patch_%s' % html.split('Change <a')[1].split('</a>')[0].split('">')[-1]
 
@@ -68,7 +70,7 @@ def get_content(patch_url):
 
 
 def create_file(project_name, bug_name, patch_url, patch_num=1):
-    filename = '%s_%s_PS%s.txt' % (project_name, bug_name, str(patch_num))
+    filename = '%s_%s_PS%s.txt' % (project_name, bug_name, patch_num)
     print 'Getting patch', patch_num
     while True:
         try:
@@ -109,7 +111,7 @@ def main():
                 html = get_html(url)
                 project_name = get_project_name(html)
                 bug_name = get_bug_name(html)
-                commit_urls = get_commit_urls(url, html)
+                patch_urls = get_patch_urls(url, html)
             except IndexError:
                 print "Get html failed! Retrying..."
                 continue
@@ -117,18 +119,18 @@ def main():
             break
         print 'Project:', project_name
         print 'Bug name:', bug_name
-        print 'PS count:', len(commit_urls)
+        print 'PS count:', len(patch_urls)
         if isinstance(bug_name, tuple):
             directory = create_folder(project_name, bug_name[0])
             os.chdir("/".join([os.getcwd(), directory]))
             bug_name = bug_name[1]
-        if len(commit_urls) == 1:
-            create_file(project_name, bug_name, commit_urls[1])
+        if len(patch_urls) == 1:
+            create_file(project_name, bug_name, patch_urls[1])
         else:
             directory = create_folder(project_name, bug_name)
             os.chdir("/".join([os.getcwd(), directory]))
-            for patch_num in commit_urls:
-                create_file(project_name, bug_name, commit_urls[patch_num], patch_num)
+            for patch_num, patch_url in patch_urls:
+                create_file(project_name, bug_name, patch_url, patch_num)
 
 
 if __name__ == "__main__":
